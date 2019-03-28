734f04c4df733e30afd8d65aa2ffaf04f6aae61b
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-03-28 21:59:05 +0200

    Initial commit

---
+++ src/test/java/org/fasttrackit/onlineshopapi/ProductServiceIntegrationTests.java
@@ -0,0 +1,36 @@
+package org.fasttrackit.onlineshopapi;
+
+import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.service.ProductService;
+import org.fasttrackit.onlineshopapi.transfer.CreateProductRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceIntegrationTests {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void testCreateProduct_whenValidRequest_thenReturnProductWithId() {
+        CreateProductRequest request = new CreateProductRequest();
+        request.setName("Laptop");
+        request.setPrice(10);
+        request.setQuantity(3);
+        request.setSku("fdas321k231");
+
+        Product product = productService.createProduct(request);
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), greaterThan(0L));
+    }
+}
