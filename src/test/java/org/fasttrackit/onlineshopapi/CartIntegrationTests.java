72a012f287812d673dcc2839a53e1bf8e879fd9a
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2019-05-10 19:21:05 +0300

    finished implementing OneToOne and ManyToMany relationships

--- src/test/java/org/fasttrackit/onlineshopapi/CartIntegrationTests.java
+++ src/test/java/org/fasttrackit/onlineshopapi/CartIntegrationTests.java
@@ -3,12 +3,11 @@ package org.fasttrackit.onlineshopapi;
 import org.fasttrackit.onlineshopapi.domain.Cart;
 import org.fasttrackit.onlineshopapi.domain.Customer;
 import org.fasttrackit.onlineshopapi.domain.Product;
+import org.fasttrackit.onlineshopapi.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshopapi.service.CartService;
 import org.fasttrackit.onlineshopapi.steps.CustomerSteps;
 import org.fasttrackit.onlineshopapi.steps.ProductSteps;
 import org.fasttrackit.onlineshopapi.transfer.cart.SaveCartRequest;
-import org.fasttrackit.onlineshopapi.transfer.customer.CustomerIdentifier;
-import org.fasttrackit.onlineshopapi.transfer.product.ProductIdentifier;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,23 +35,21 @@ public class CartIntegrationTests {
     private CustomerSteps customerSteps;
 
     @Test
-    public void testAddProductsToCart_whenValidRequest_thenReturnCart() {
+    public void testAddProductsToCart_whenValidRequest_thenReturnCart() throws ResourceNotFoundException {
         Product product = productSteps.createProduct();
         Customer customer = customerSteps.createCustomer();
 
-        ProductIdentifier productIdentifier = new ProductIdentifier();
-        productIdentifier.setId(product.getId());
-
-        CustomerIdentifier customerIdentifier = new CustomerIdentifier();
-        customerIdentifier.setId(customer.getId());
-
         SaveCartRequest request = new SaveCartRequest();
-        request.setCustomer(customerIdentifier);
-        request.setProducts(Collections.singleton(productIdentifier));
+        request.setCustomerId(customer.getId());
+        request.setProductIds(Collections.singleton(product.getId()));
 
         Cart cart = cartService.addProductsToCart(request);
 
-        // todo: add assertions
+        assertThat(cart, notNullValue());
+        assertThat(cart.getId(), is(customer.getId()));
+
+        assertThat(cart.getProducts(), notNullValue());
+        assertThat(cart.getProducts(), hasSize(1));
     }
 
 }
